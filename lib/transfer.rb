120d510e4a57695b72669f6f37fb0b074e3b8586
dowdje <dowdje89@gmail.com>
2016-07-31 12:39:57 -0400

    Completed oo-banking lab.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  def initialize(sender, receiver, amount)
+    @status = 'pending'
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if @status == 'pending' && self.valid? && sender.balance >= amount
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = 'complete'
+  else
+    @status = 'rejected'
+    "Transaction rejected. Please check your account balance."
+  end
+  end
+
+  def reverse_transfer
+    if @status == 'complete'
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = 'reversed'
+    end
+  end
 end
